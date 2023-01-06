53fe4083e0831f40f1323cf9166a308e3c184f0c
Eduards <eduards.abisevs@elev.ga.ntig.se>
2023-01-06 17:54:00 +0100

    added pi-camera module

--- flask-ard-car.py
+++ flask-ard-car.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, abort
+from flask import Flask, render_template, request, Response
 import serial, time
-
+from camera import VideoCamera
 app = Flask(__name__)
+
+pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
 
 # arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
 def write_to_arduino(id):
@@ -12,7 +14,25 @@ def write_to_arduino(id):
 
 @app.route("/")
 def home_page():
-    return render_template('base.html') 
+    return render_template('index.html') 
+
+def gen(camera):
+    #get camera frame
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(gen(pi_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Take a photo when pressing camera button
+@app.route("/picture")
+def take_picture():
+    pi_camera.take_picture()
+    return "None"
 
 @app.route("/send")
 def retrieve_data():
@@ -23,11 +43,8 @@ def retrieve_data():
     elif 6 <= id <= 7:
         pass # do the speed logic here.
     else:
-        return abort(404)
+        return print(f"Number: {id} is out of the range") 
     return "success"
-@app.route("/video_feed")
-def video_feed():
-    pass #logic for live video
-    return "smth"
+
 if __name__ == "__main__":
     app.run(debug=True)
